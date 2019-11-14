565fde573c01dbbd17db8debfda030f166ed41c2
Robert Crous <robert.crous21@gmail.com>
2019-11-14 12:10:05 +0200

    Add eslint-plugin-button-has-type plugin

---
+++ README.md
@@ -0,0 +1,51 @@
+# eslint-plugin-react-button-has-type
+
+Checks that all buttons have a set type attribute
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-react-button-has-type`:
+
+```
+$ npm install eslint-plugin-react-button-has-type --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-react-button-has-type` globally.
+
+## Usage
+
+Add `react-button-has-type` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "react-button-has-type"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "react-button-has-type/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/react-button-has-type.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview ba
+ * @author ba
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var RuleTester = require("eslint").RuleTester;
+
+var rule = require("../../../lib/rules/react-button-has-type");
+
+
+var parserOptions = {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+        jsx: true
+    }
+};
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester({ parserOptions });
+ruleTester.run("react-button-has-type", rule, {
+
+    valid: [
+        "<button type='button'></button>",
+    ],
+
+    invalid: [
+        {
+            code: "<button></button>",
+            errors: [{
+                message: "Missing type attribute on button",
+                type: "JSXElement"
+            }]
+        },
+        {
+            code: "<button type=''></button>",
+            errors: [{
+                message: "Button type property is empty",
+                type: "JSXElement"
+            }]
+        },
+        {
+            code: "<button type='' type='exists'>hello world!</button>",
+            errors: [{
+                message: "Button type property is empty",
+                type: "JSXElement"
+            }]
+        },
+        {
+            code: "<div><span><button type='' type='exists'>hello world!</button></span></div>",
+            errors: [{
+                message: "Button type property is empty",
+                type: "JSXElement"
+            }]
+        }
+    ]
+});
